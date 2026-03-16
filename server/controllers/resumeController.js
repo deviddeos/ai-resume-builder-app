c13504818e7f6abfc04488183d27ed8519e53e48
Prince Gupta <20prince22@gmail.com>
2026-03-17 02:27:51 +0530

    integrated ai API and created routes of AI

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,4 +1,7 @@
+import imagekit from "../configs/imagekit.js";
 import Resume from "../models/Resume.js";
+import fs from 'fs'
+
 
 
 // controller for creating new resume
@@ -76,5 +79,39 @@ export const getPublicResumeById = async (req, res) =>{
 
     } catch (error) {
         return res.status(400).json({message: error.message})
+    }
+}
+
+
+// controller for updating a resume
+// PUT: /api/resumes/update
+export const updateResume = async(req, res)=>{
+    try {
+        const userId = req.userId;
+        const {resumeId, resumeData, removeBackground} = req.body
+        const image = req.file;
+
+        let resumeDataCopy = JSON.parse(resumeData);
+
+        if(image){
+            const imageBufferData = fs.createReadStream(image.path)
+            const response = await imagekit.files.upload({
+                file: imageBufferData,
+                fileName: 'resume.png',
+                folder: 'user-resumes',
+                transformation: {
+                    pre: 'w-300, h300, fo-face, z-0.75' + 
+                    (removeBackground ? ',e-bgremove' : '')
+                }
+            });
+            resumeDataCopy.personal_info.image = response.url
+        }
+
+        const resume = await Resume.findByIdAndUpdate({userId, _id: resumeId}, resumeDataCopy, {new: true})
+        return res.status(200).json({message: 'Saved successfully', resume})
+
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+
     }
 }
---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { enhanceJobDescription, enhanceProfessionalSummary, uploadResume } from '../controllers/aiController';
+
+const aiRouter = express.Router();
+
+aiRouter.post('/enhance-pro-sum', protect, enhanceProfessionalSummary)
+aiRouter.post('/enhance-job-desc', protect, enhanceJobDescription)
+aiRouter.post('/upload-resume', protect, uploadResume)
+
+export default aiRouter;
